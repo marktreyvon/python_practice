7c09f5557ec9b1299d66e56883e14ec95240dc72
markt <marktreyvon@gmail.com>
2019-03-18 15:28:05 +0800

    ddd

--- autoupload.py
+++ autoupload.py
@@ -1,10 +1,11 @@
-import os
+import os,time
 o1 = os.popen('git add .')
 print(o1.read())
 inp = input()
+time.sleep(1)
 o2 = os.popen('git commit -m ' + inp)
 print(o2.read())
 
 o3 = os.popen('git push')
 # o3.write('githubMIMA666!')
-print(o3)
+print(o3.read())
